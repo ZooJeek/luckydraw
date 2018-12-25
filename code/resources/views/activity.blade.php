ce3a8827919eeeb514fe7c416e8d20c84763e517
wkosea <1254843177@qq.com>
2018-12-25 22:09:24 +0800

    Update activity.blade.php

--- code/resources/views/activity.blade.php
+++ code/resources/views/activity.blade.php
@@ -6,12 +6,12 @@
 当前收获信息：
 {{ $address ?? "未填写" }}<br><br>
 
-恭喜您获得奖励，请填写收货信息:<br>
+恭喜您获得奖励，请填写收货信息（可使用化名）:<br>
 <form action="/address" method="post">
 	@csrf
 	<textarea rows="3" cols="20" name="address">
 
-	</textarea>
+	</textarea><br>
 	<button type="submit" class="nes-btn" style="margin-top: 10px;"> 提交 </button>
 </form>
 
